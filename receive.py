1a3480a52257ee7da462f0cd92868b88f2e2c09f
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-12-27 02:33:24 -0300

    rename queue

--- receive.py
+++ receive.py
@@ -16,11 +16,11 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.queue_declare(queue="hello")
+    channel.queue_declare(queue="persons_data")
 
     callback = lambda ch, method, properties, body: print_data(body)
 
-    channel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue="persons_data", on_message_callback=callback, auto_ack=True)
 
     print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
